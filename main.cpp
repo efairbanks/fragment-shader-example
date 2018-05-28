b6cd13400afbfc04d03a9de5b914c4711827f397
Eric Fairbanks <eric.p.fairbanks@gmail.com>
2018-05-28 01:52:29 -0400

    Now with time, ray marching, and screen tearing!

--- main.cpp
+++ main.cpp
@@ -29,6 +29,8 @@ char* loadFile(char* file_name){
   return file_contents;
 }
 int uniform_resolution;
+int uniform_time;
+double time=0;
 void reshape(int width, int height){
   if(uniform_resolution!=-1) glUniform2f(uniform_resolution,width,height);
   glViewport(0,0,width,height);
@@ -40,6 +42,12 @@ void display(void){
   //glUseProgram(0);
   glutSwapBuffers();
 }
+void timer(int i){
+  glutPostRedisplay();
+  time+=i*0.001;
+  if(uniform_time!=-1) glUniform1f(uniform_time,time);
+  glutTimerFunc(i,timer,i);
+}
 int main(int argc,char** argv){
   char* f=loadFile("simple.fragment");
   int flen=strlen(f);
@@ -47,7 +55,7 @@ int main(int argc,char** argv){
   printf("%s\n\n",f);
   glutInit(&argc, argv);
   int windowID=glutCreateWindow("single triangle");
-  glClearColor(0,0,1,1);
+  glClearColor(0,0,0,1);
   // --- //
   programId = glCreateProgram();
   if(!programId) printf("glCreateProgram Error\n");
@@ -59,9 +67,11 @@ int main(int argc,char** argv){
   glLinkProgram(programId);
   glUseProgram(programId);
   uniform_resolution=glGetUniformLocation(programId,"resolution");
+  uniform_time=glGetUniformLocation(programId,"time");
   // --- //
   glutReshapeFunc(reshape);
   glutDisplayFunc(display);
+  timer(1000/60);
   glutMainLoop(); 
   return 0;
 }
